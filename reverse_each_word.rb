c70d32f0c6af3e16a22501d89e2559e0a54cf1e3
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-21 17:54:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
-def reverse_each_word
-  reverse_each_word.each do |revers| 
-    
-    end
+def reverse_each_word(string)
+  string = string.split
+  string.collect do |word|
+    word.reverse!
+  end
+  string.join(" ")
 end
